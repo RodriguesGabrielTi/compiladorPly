fa3644ea7db9c0af2cdb82568d6e1589b05fe515
João Pedro Cardoso Barbosa <joao.c.barbosa2002@gmail.com>
2022-11-29 11:48:54 -0300

    Analisador léxico
    * Foi definido o analisador léxico com uma sintaxe semelhante a C, incluindo os comentários
    * Dentro do arquivo está contido o exemplo do professor, com os tokens definidos corretamente

---
+++ lang_lex.py
@@ -0,0 +1,123 @@
+from ply.lex import lex
+
+# --- TOKENIZER
+
+# PALAVRAS RESERVADAS
+reserved = ('def', 'void', 'int', 'float', 'str', 'return', 'if')
+
+tokens = reserved + ('RPAREN', 'LPAREN', 'RBRACES', 'LBRACES', 'RBRACK', 'LBRACK',
+                     'COMMA', 'SEMICOL', 'COLON',
+                     'EQUALS', 'MINUS', 'PLUS', 'TIMES', 'DIVIDE', 'OR', 'AND', 'NOT', 'LT', 'LE', 'GT', 'GE', 'EQ',
+                     'NE',
+                     'COMMENT',
+                     'IDENT', 'STRCONST', 'INTCONST', 'FLOATCONST')
+
+# Espaços e Tabs
+t_ignore = ' \t'
+
+# DELIMITADORES
+t_RPAREN = r'\('
+t_LPAREN = r'\)'
+t_RBRACES = r'\{'
+t_LBRACES = r'\}'
+t_RBRACK = r'\['
+t_LBRACK = r'\]'
+
+# PONTUAÇÃO
+t_COMMA = r','
+t_SEMICOL = r';'
+t_COLON = r':'
+
+# OPERAÇÕES
+t_EQUALS = r'='
+t_MINUS = r'-'
+t_PLUS = r'\+'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_OR = r'\|\|'
+t_AND = r'&&'
+t_NOT = r'!'
+t_LT = r'<'
+t_GT = r'>'
+t_LE = r'<='
+t_GE = r'>='
+t_EQ = r'=='
+t_NE = r'!='
+
+# SIMBOLOS NÃO TRIVIAIS
+t_STRCONST = r'([\"][^\"]*[\"])'
+
+
+def t_IDENT(t):
+    r'[a-zA-Z]([a-zA-Z]|[\d]|[\_])*'
+    if t.value in reserved:
+        t.type = t.value
+    return t
+
+
+def t_INTCONST(t):
+    r'(\+|-)?\d+'
+    t.value = int(t.value)
+    return t
+
+
+def t_FLOATCONST(t):
+    r'(\+|-)?[\d]+([\.])?[\d]+'
+    t.value = float(t.value)
+    return t
+
+
+# Ignored token with an action associated with it
+def t_ignore_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count('\n')
+
+
+# Comment (C-Style)
+def t_COMMENT(t):
+    r'/\*(.|\n)*?\*/'
+    t.lexer.lineno += t.value.count('\n')
+    return t
+
+
+# Error handler for illegal characters
+def t_error(t):
+    print(f'Illegal character {t.value[0]!r}')
+    t.lexer.skip(1)
+
+
+# Build the lexer object
+lexer = lex()
+if __name__ == "__main__":
+    # lex.runmain(lexer)
+
+    # Teste exemplo do professor
+    data = '''
+    def func1( int A , int B )
+    {
+        int SM [2];
+        SM [0] = A + B ;
+        SM [1] = B * C ;
+        return;
+    }
+    def principal()
+    {
+    int C;
+    int D;
+    int R;
+    C = 4;
+    D = 5;
+    R = func1 (C , D );
+    return;
+    }
+    '''
+
+    # Give the lexer some input
+    lexer.input(data)
+
+    # Tokenize
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break  # No more input
+        print(tok)
